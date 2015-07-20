697339eb8db4692280f533b5ee7b9ef59ac1c222
Querci Luca <lucabit73@gmail.com>
2015-07-20 11:08:13 +0200

    simple test with js(log remove, minified) and sass file

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
 $( document ).ready(function() {
-    
+    $('#test').on('click', function(){
+        console.log('test click');
+        $(this).toggleClass('click');
+    });
+    console.log('document load');
 });
